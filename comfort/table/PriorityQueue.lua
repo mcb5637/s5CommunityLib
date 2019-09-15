de9bd8b58f37d5c283ce372d6fa9f4441ed3ceda
mcb <mgrbo@web.de>
2019-09-15 17:36:19 +0200

    renamed priorityqueue

--- comfort/table/PriorityQueue.lua
+++ comfort/table/PriorityQueue.lua
@@ -4,21 +4,21 @@ end --mcbPacker.ignore
 
 
 --- author:mcb		current maintainer:mcb		v1.0
--- Datenstruktur priorityQueue als OOP.
+-- Datenstruktur PriorityQueue als OOP.
 -- 
--- q = priorityQueue:init()			Gibt eine neue leere priorityQueue zurück
--- q:add(a, v)						Fügt den Wert a mit der gewichtung v hinzu.
--- q:remove()						Entfernt den Wert mit der geringsten Gewichtung und gibt ihn zurück.
--- q:isEmpty()						Gibt zurück, ob die priorityQueue leer ist.
--- q:decrease(a, v)					Verringert die gewichtung von a auf v.
+-- q = PriorityQueue:New()			Gibt eine neue leere PriorityQueue zurück
+-- q:Add(a, v)						Fügt den Wert a mit der gewichtung v hinzu.
+-- q:Remove()						Entfernt den Wert mit der geringsten Gewichtung und gibt ihn zurück.
+-- q:IsEmpty()						Gibt zurück, ob die PriorityQueue leer ist.
+-- q:Decrease(a, v)					Verringert die gewichtung von a auf v.
 -- 
 -- Benötigt:
 -- - CopyTable
-priorityQueue = {vals={}}
-function priorityQueue:init()
+PriorityQueue = {vals={}}
+function PriorityQueue:New()
 	return CopyTable(self)
 end
-function priorityQueue:add(a, v)
+function PriorityQueue:Add(a, v)
 	local ins = {v=v, a=a}
 	for i=1,table.getn(self.vals) do
 		if self.vals[i].v > v then
@@ -28,13 +28,13 @@ function priorityQueue:add(a, v)
 	end
 	table.insert(self.vals, ins)
 end
-function priorityQueue:remove()
+function PriorityQueue:Remove()
 	return table.remove(self.vals, 1).a
 end
-function priorityQueue:isEmpty()
+function PriorityQueue:IsEmpty()
 	return not self.vals[1]
 end
-function priorityQueue:decrease(a, v)
+function PriorityQueue:Decrease(a, v)
 	for i=1,table.getn(self.vals) do
 		if self.vals[i].a == a then
 			table.remove(self.vals, i)
