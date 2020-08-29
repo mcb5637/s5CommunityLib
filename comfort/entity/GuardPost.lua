85dbc05b9fc9d247e619e417904b60c7bc6d8542
mcb <mgrbo@web.de>
2020-08-29 21:35:12 +0200

    added GuardPost

---
+++ comfort/entity/GuardPost.lua
@@ -0,0 +1,130 @@
+if mcbPacker then --mcbPacker.ignore
+mcbPacker.require("s5CommunityLib/lib/UnlimitedArmy")
+mcbPacker.require("s5CommunityLib/fixes/TriggerFix")
+mcbPacker.require("s5CommunityLib/comfort/math/GetDistance")
+mcbPacker.require("s5CommunityLib/comfort/pos/IsInCone")
+mcbPacker.require("s5CommunityLib/comfort/other/S5HookLoader")
+mcbPacker.require("s5CommunityLib/comfort/other/PredicateHelper")
+mcbPacker.require("s5CommunityLib/lib/MemoryManipulation")
+end --mcbPacker.ignore
+
+
+--- author:mcb		current maintainer:mcb		v0.1b
+-- Einfache möglichkeit um wachposten zu realisieren.
+-- 
+-- - GuardPost.AddGuard(ids, range, cone, targets, callback, deadcallback, ...)			erzeugt einen/mehrere wachen.
+-- - ids			eine id/scriptname oder ein table von mehreren, die wachen.
+-- - range			sichtreichweite der wachen.
+-- - cone			der winkel von der richtung der wache in dem bemerkt wird (2 seitig).
+-- - targets		ein table mit zielen, oder playern (mit targets.player gesetzt), oder nil für alle feinde der ersten wache (zum zeitpunkt des aufrufes).
+-- - callback		func(id, tid, unpack(arg))		wird aufgerufen, wenn die wache etwas bemerkt, id ist die wache, tid das bemerkte ziel.
+-- - deadcallback	func(unpack(arg)) oder nil		wird aufgerufen, wenn alle wachen tot sind (optional).
+-- gibt der callback true zurück, wird der job beendet.
+-- 
+-- Benötigt:
+-- - UnlimitedArmy (Zielprüfung)
+-- - GetDistance
+-- - IsInCone
+-- - TriggerFix
+-- - S5Hook (optional, verbessert erkennung)
+-- - PredicateHelper (nur bei Hook)
+-- - MemoryManipulation (nur bei Hook)
+GuardPost = {}
+
+function GuardPost.AddGuard(ids, range, cone, targets, callback, deadcallback, ...)
+	if type(ids)~="table" then
+		ids = {ids}
+	end
+	for i=table.getn(ids),1,-1 do
+		if IsDead(ids[i]) then
+			table.remove(ids, i)
+		end
+	end
+	assert(IsAlive(ids[1]), "no valid guard")
+	assert(range > 0)
+	if not targets then
+		targets = {player=true}
+		local l = GetPlayer(ids[1])
+		for i=1,8 do
+			if Logic.GetDiplomacyState(l, i)==Diplomacy.Hostile then
+				table.insert(targets, i)
+			end
+		end
+	end
+	local t = {
+		ids = ids,
+		range = range,
+		cone = cone,
+		targets = targets,
+		callback = callback,
+		deadcallback = deadcallback,
+		arg = arg,
+	}
+	if S5Hook then
+		t.CheckArea = GuardPost.CheckAreaHook
+	else
+		t.CheckArea = GuardPost.CheckAreaNoHook
+	end
+	return StartSimpleHiResJob("GuardPost.Job", t)
+end
+
+function GuardPost.Job(t)
+	for i=table.getn(t.ids),1,-1 do
+		local id = GetID(t.ids[i])
+		if IsDead(id) then
+			table.remove(t.ids, i)
+		else
+			local r = Logic.GetEntityOrientation(id)
+			if t.targets.player then
+				if t.CheckArea(t, id, r) then
+					return true
+				end
+			else
+				for j=table.getn(t.targets),1,-1 do
+					local tid = GetID(t.targets[j])
+					if IsDead(tid) then
+						table.remove(t.targets, j)
+					elseif GetDistance(id, tid)<= t.range and IsInCone(tid, id, r, t.cone) and GuardPost.IsValidTarget(tid) then
+						if t.callback(id, tid, unpack(t.arg)) then
+							return true
+						end
+					end
+				end
+			end
+		end
+	end
+	if not t.ids[1] then
+		if t.deadcallback then
+			t.deadcallback(unpack(t.arg))
+		end
+		return true
+	end
+end
+
+function GuardPost.CheckAreaHook(t, id, r)
+	local p = GetPosition(id)
+	for tid in S5Hook.EntityIterator(Predicate.OfAnyPlayer(unpack(t.targets)), PredicateHelper.GetETypePredicate(UnlimitedArmy.EntityTypeArray), Predicate.InCircle(p.X, p.Y, t.range)) do
+		if IsInCone(tid, p, r, t.cone) and GuardPost.IsValidTarget(tid) then
+			if t.callback(id, tid, unpack(t.arg)) then
+				return true
+			end
+		end
+	end
+end
+
+function GuardPost.CheckAreaNoHook(t, id, r)
+	local p = GetPosition(id)
+	local data = {Logic.GetPlayerEntitiesInArea(GetPlayer(id), 0, p.X, p.Y, r, 16)}
+	table.remove(data, 1)
+	for _,tid in ipairs(data) do
+		if IsInCone(tid, p, r, t.cone) and GuardPost.IsValidTarget(tid) then
+			if t.callback(id, tid, unpack(t.arg)) then
+				return true
+			end
+		end
+	end
+end
+
+function GuardPost.IsValidTarget(id)
+	return UnlimitedArmy.IsValidTarget(id, nil, nil)
+end
