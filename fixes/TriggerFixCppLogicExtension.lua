a6f321df4bae86e98586d20b083098d85f4ded7f
mcb <mgrbo@web.de>
2021-05-06 23:08:27 +0200

    better conversion cb

--- fixes/TriggerFixCppLogicExtension.lua
+++ fixes/TriggerFixCppLogicExtension.lua
@@ -7,7 +7,7 @@ if not CppLogic then
     return
 end
 
-TriggerFixCppLogicExtension = {CreatedTrigger=nil, FirstCreated=nil}
+TriggerFixCppLogicExtension = {CreatedTrigger=nil}
 
 TriggerFix.AddScriptTrigger("SCRIPT_EVENT_ON_CONVERT_ENTITY")
 
@@ -20,29 +20,13 @@ function TriggerFixCppLogicExtension.AddLeaveTrigger()
     return true
 end
 
-function TriggerFixCppLogicExtension.Hook(targetId, player, isPost, converterId)
-    if not isPost then
-        TriggerFixCppLogicExtension.CreatedTrigger = Trigger.RequestTrigger(Events.LOGIC_EVENT_ENTITY_CREATED, nil, TriggerFixCppLogicExtension.OnCreate, 1)
-    else
-        if TriggerFixCppLogicExtension.FirstCreated then
-            local ev = TriggerFix.CreateEmptyEvent()
-            ev.GetEntityID1 = targetId
-            ev.GetEntityID2 = TriggerFixCppLogicExtension.FirstCreated
-            ev.GetEntityID = converterId
-            ev.GetPlayerID = player
-            TriggerFix_action(Events.SCRIPT_EVENT_ON_CONVERT_ENTITY, ev)
-            TriggerFixCppLogicExtension.FirstCreated = nil
-        else
-            EndJob(TriggerFixCppLogicExtension.CreatedTrigger)
-        end
-    end
-end
-
-function TriggerFixCppLogicExtension.OnCreate()
-    if not TriggerFixCppLogicExtension.FirstCreated then
-        TriggerFixCppLogicExtension.FirstCreated = Event.GetEntityID()
-    end
-    return true
+function TriggerFixCppLogicExtension.Hook(targetId, player, newid, converterId)
+    local ev = TriggerFix.CreateEmptyEvent()
+    ev.GetEntityID1 = targetId
+    ev.GetEntityID2 = newid
+    ev.GetEntityID = converterId
+    ev.GetPlayerID = player
+    TriggerFix_action(Events.SCRIPT_EVENT_ON_CONVERT_ENTITY, ev)
 end
 
 AddMapStartAndSaveLoadedCallback("TriggerFixCppLogicExtension.Init")
