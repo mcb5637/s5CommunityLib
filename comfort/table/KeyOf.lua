ed79f5d42c0f0dbf903d9c6a84a7b764cbf21fd2
mcb <mgrbo@web.de>
2024-02-22 21:38:59 +0100

    doc

--- comfort/table/KeyOf.lua
+++ comfort/table/KeyOf.lua
@@ -1,12 +1,16 @@
-
 --- author: ???		current maintainer: mcb			v1.0
---  gibt den key zurück unter dem _wert gespeichert ist
+--- gibt den key zurück unter dem _wert gespeichert ist
+---@generic K
+---@generic V
+---@param _wert V
+---@param _table {[K]:V}
+---@return K?
 function KeyOf(_wert, _table)
 	if _table == nil then return false end
 	for k, v in pairs(_table) do
-		if v == _wert then 
+		if v == _wert then
 			return k
-		end 
+		end
 	end
 	return nil
 end
